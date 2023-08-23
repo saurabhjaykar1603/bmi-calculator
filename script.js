bd20b9abb2cdc5b47fb228ce3976387bff24a679
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2023-08-23 16:48:28 +0530

    feat - reset progress remark

--- script.js
+++ script.js
@@ -10,18 +10,26 @@ function calculateBmi() {
     const bmi = (weight / (hm * hm)).toFixed(2);
     bmiText.innerHTML = ` ${bmi}`;
     if (bmi < 18.5) {
+        document.getElementById("progres-remark").innerHTML = "Underweight";
+
         console.log("Underweight");
     }
     else if (bmi >= 18.5 && bmi <= 24.9) {
         console.log("Normal");
+        document.getElementById("progres-remark").innerHTML = "Normal";
+
     }
 
     else if (bmi >= 25 && bmi <= 29.9) {
         console.log("Overweight");
+        document.getElementById("progres-remark").innerHTML = "Overweight";
+
     }
 
     else if (bmi >= 30) {
         console.log("Obese");
+        document.getElementById("progres-remark").innerHTML = "Obese";
+
 
 
     }
@@ -32,4 +40,10 @@ function resetBmi() {
     inputHeigtBox1.value = " ";
     inputWeightBox2.value = " ";
     bmiText.innerHTML =  " ";
+    document.getElementById("progres-remark").innerHTML = " ";
+   
+
+
+
 }
+
